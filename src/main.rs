b1ab68b047eac94669aa442b46aa7b66fc2086b9
Fernando Sckaff <sckafffernando@gmail.com>
2024-10-08 22:51:42 -0700

    Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Hello Egui",
+        options,
+        Box::new(|_cc| Ok(Box::new(MyApp::default()))),
+    )
+}
+
+#[derive(Default)]
+struct MyApp {
+    counter: i32,
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello, Egui!");
+
+            if ui.button("Increment Counter").clicked() {
+                self.counter += 1;
+            }
+            ui.label(format!("Counter: {}", self.counter));
+        });
+    }
 }
